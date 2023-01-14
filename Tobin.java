01d98f685d57fe62fc69845770ad85010806a2fe
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2023-01-14 16:13:13 +0530

    Update Tobin.java
    add comments

--- Tobin.java
+++ Tobin.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
 
-//your code goes here
-
-
 public class Tobin {
     public static void main(String[ ] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        System.out.print(Convert.toBinary(x));
+        Scanner sc = new Scanner(System.in);//creating scanner object
+        int x = sc.nextInt(); //take decimal number 
+        System.out.print(Convert.toBinary(x)); //call binary converter method and display value
     }
 }
+
 public class Convert{
+    //public class for return string binary value 
     public static String toBinary(int x){
-        String binary = "";
+        String binary = "";//declare binary variable String
+      
+        //convertion of binary
         while(x>0){
             binary = (x % 2)+binary;
             x /= 2;
         }
-        return binary;
+        return binary;//return String value of binary 
     }
 }
